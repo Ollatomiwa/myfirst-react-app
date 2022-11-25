283c61f9c1bafaaa0d294304e40c10a1a463e662
Ollatomiwa <ajaomoshood@gmail.com>
2022-11-25 15:43:28 +0000

    starting point

---
+++ src/components/counter.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    //constructor(props) {
+       //super(props);
+    
+    render() { 
+    
+       return <div><h1> hello world</h1><button>Increment</button></div>;
+    }
+}
+ 
+export default Counter;
