023981e3bca8dda2dd8ec8a0188783077a9c8db3
Ollatomiwa <ajaomoshood@gmail.com>
2022-11-26 10:30:24 +0000

    we updated the state: once the incement button is clicked the value will increase

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,46 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    //constructor(props) {
-       //super(props);
+    //constructor() {
+      // super();
+       //this.handleIncrement = this.handleIncrement.bind(this);
+    //}
+    state ={
+      count: 1,
+      tags: ['tag1', 'tag2', 'tag3']
+
+    };
+    handleIncrement=()=>{
+     this.setState({count: this.state.count +1}) // updating the state: if we click on button the value will increase// 
+     
+   }
+   
+    render()
+   { 
     
-    render() { 
-    
-       return <div><h1> hello world</h1><button>Increment</button></div>;
-    }
+      return ( 
+         <div>
+          <span className={this.getBadgeClasses()}> {this.formatCount()}</span>
+            <button 
+            onClick={this.handleIncrement}
+             className="btn btn-secondary btn-sm"> Increment
+            </button>
+            <ul>{this.state.tags.map (tag => <li key={tag}>{tag}</li>)}</ul> 
+         </div>); 
+   }
+  
+
+   getBadgeClasses() {
+      let classes= " badge  m-2 badge-";
+      classes += this.state.count === 0 ? " bg-warning" : "  bg-primary";
+      return classes; 
+
+   }
+    formatCount()
+   {
+      const {count}= this.state;
+      return count === 0 ? "Zero": count;
+   }
 }
  
 export default Counter;
